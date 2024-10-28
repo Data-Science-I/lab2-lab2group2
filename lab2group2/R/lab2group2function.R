881107df1772883b471667f7e4479cf3fefeb6a9
Jing Lin <jinglin702@gmail.com>
2024-10-28 17:24:34 -0400

    R package+Function1+Testing

---
+++ lab2group2/R/lab2group2function.R
@@ -0,0 +1,37 @@
+#' Generate Boxplot by DRG Code
+#'
+#' This function generates a boxplot of payments by DRG code.
+#' Users can choose between average Medicare payments, average total payments, or average covered charges.
+#'
+#' @param data Data frame containing DRG data.
+#' @param payment_type A string indicating the type of payment to plot: "medicare", "total", or "covered".
+#' @return A ggplot object of the boxplot.
+#' @examples
+#' boxplot_function(drg_data, "medicare")
+#' @export
+boxplot_function <- function(data, payment_type) {
+  payment_column=case_when(
+    payment_type=="medicare" ~ "Average.Medicare.Payments",
+    payment_type=="total" ~ "Average.Total.Payments",
+    payment_type=="covered" ~ "Average.Covered.Payments"
+  )
+
+  # Check if the chosen column exists in the data
+  if (!payment_column %in% names(data)) {
+    stop("The specified payment type does not exist in the data.")
+  }
+
+  #add column to data that has just the code numbers
+  new_data <- data |>
+    mutate(DRG_code=substr(DRG.Definition, 1, 3))
+
+  # Create the boxplot
+  ggplot(new_data, aes_string(x = "DRG_code", y = payment_column)) +
+    geom_boxplot() +
+    labs(title = paste("Boxplot of", payment_type, "payments by DRG Code"),
+         x = "DRG Code",
+         y = paste("Average", payment_type, "payments")) +
+    theme_minimal()+
+    theme(axis.text.x = element_text(angle = 90, size=6))
+
+}
